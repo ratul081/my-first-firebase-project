aa4959e0a5d57102e8460e5152f444bf1b842c6f
ratul081 <ratullinux77@gmail.com>
2023-05-12 21:09:04 +0600

    Done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,48 @@
-import logo from './logo.svg';
 import './App.css';
+import { GoogleAuthProvider, getAuth, signInWithPopup, signOut } from 'firebase/auth'
+import app from './firebase/firebase.init';
+import { useState } from 'react';
+
+const auth = getAuth(app);
 
 function App() {
+  const googleProvider = new GoogleAuthProvider();
+  const [user, setUser] = useState({});
+
+  const handleGoogleSingIn = () => {
+    signInWithPopup(auth, googleProvider)
+      .then(result => {
+        const user = result.user;
+        setUser(user);
+      })
+      .catch(err => {
+        console.error("error", err);
+      })
+  }
+
+  const handleSingOut = () => {
+    signOut(auth)
+      .then(() => {
+        setUser({})
+      })
+      .catch(() => {
+        setUser({})
+      })
+  }
+
+  console.log("🚀 ~ file: App.js:11 ~ App ~ user:", user)
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {user.uid ? <button onClick={handleSingOut} >Google logout</button>
+        : <button onClick={handleGoogleSingIn} >Google login</button>
+      }
+
+      {user.uid && <>
+        <h1>User Name: {user.displayName}</h1>
+        <p>Email: {user.email}</p>
+        <img src={user.photoURL} alt='' />
+      </>
+      }
     </div>
   );
 }
---
+++ src/firebase/firebase.init.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBD5iDzL84gXLznweDLEdcN0TGsFAGJ6qU",
+  authDomain: "my-frist-firebase-app-2b157.firebaseapp.com",
+  projectId: "my-frist-firebase-app-2b157",
+  storageBucket: "my-frist-firebase-app-2b157.appspot.com",
+  messagingSenderId: "41453348236",
+  appId: "1:41453348236:web:be821d2c47b9118e1ee2a9"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app; 
